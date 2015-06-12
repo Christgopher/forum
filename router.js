d69c189806ce428e4157e122efc5f93f35e98b9d
christopher le compte <christgopher@gmail.com>
2015-06-12 11:37:43 -0700

    add editing and deleting

--- router.js
+++ router.js
@@ -1,4 +1,7 @@
 Forum.Router.map(function() {
   this.resource('questions', {path: '/'});
   this.resource('new-question');
+  this.resource('question', {path: ':question_id'}, function() {
+    
+  });
 });
